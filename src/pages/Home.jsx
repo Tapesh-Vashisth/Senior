f1c9dc9db8ae5c481c6f0a32bc91be656f066f93
lleviackermann <supritkumar30@gmail.com>
2023-08-19 01:25:06 +0530

    navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import Box from '@mui/material/Box';
+import NavBar from "../components/NavBar";
 import '../styles/home.css'
 
 function Home() {
   return (
-    <div>
-      <div>
-        Hello
-      </div>
-      <div>
-
-      </div>
-    </div>
+    <>
+    <NavBar className="navbar" />
+    </>
   );
 }
 
