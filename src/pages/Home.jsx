ca27394f2219d21713bacd3747f2f8ce5a5c114d
tapesh <tapeshvashisth789@gmail.com>
2023-08-19 09:34:43 +0530

    useselector

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import '../styles/home.css'
 import CreateModal from "../components/CreateModal";
 import CreatePostModal from "../components/CreatePostModal";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const boards = useSelector((state) => state.boards);
+
+  useEffect(() => {
+    console.log(boards)
+  }, [])
+  
   return (
     <>
     <NavBar className="navbar" />
